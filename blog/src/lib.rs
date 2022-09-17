d920bd40f1b3d627668685d26abd1edacffb7bd3
Tamura Yukichi <tamura.yukichi@gmail.com>
2022-09-17 18:59:41 +0900

    add post

---
+++ blog/src/lib.rs
@@ -0,0 +1,84 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(&self)
+    }
+
+    pub fn request_review(&mut self) {
+        // takeメソッドを呼び出して、 stateフィールドからSome値を取り出し、その箇所にNoneを残します。
+        // なぜなら、Rustは、 構造体に未代入のフィールドを持たせてくれないからです。
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve());
+        }
+    }
+}
+
+trait State {
+    // メソッドの第1引数にself、&self、&mut selfではなく、
+    // self: Box<Self>としていることに注意してください。この記法は、
+    // 型を保持するBoxに対して呼ばれた時のみ、 このメソッドが合法になることを意味しています。
+    // この記法は、Box<Self>の所有権を奪い、古い状態を無効化するので、
+    // Postの状態値は、新しい状態に変形できます。
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn content<'a>(&self, pos: &'a Post) -> &'a str {
+        &pos.content
+    }
+}
